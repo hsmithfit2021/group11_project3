e53cd354363e143d7806e171860d907aa808331e
ChristianPrieto243 <cprieto2023@my.fit.edu>
2024-11-10 16:19:11 -0500

    Update ArticleCard.js
    Setup Articlecard componet to accept the article prop so that member 4 can render the cards

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,24 +1,31 @@
+// src/components/ArticleCard.js
+
 import '../style/ArticleCard.css';
 
-function ArticleCard() {
-    // Placeholder/Template, will need to be generated based on selected article
-    return (
-        <div class="proj-card">
-            <img />
-            <a>
-                link
-            </a>
-            <h4>
-                Title
-            </h4>
-            <p class = "date">
-                Date
-            </p>
-            <p class = "desc">
-                Description
-            </p>
-        </div>
-    );
-  }
+function ArticleCard({ article }) {
+  // Use the article prop to display article data
+  return (
+    <div className="proj-card">
+      <img src={article.card_image} alt={article.title} />
+      <a href={article.url}>{article.domain}</a>
+      <h4>{article.title}</h4>
+      <p className="date">{formatDate(article.published_date)}</p>
+      <p className="desc">{getExcerpt(article.content)}</p>
+    </div>
+  );
+}
 
-  export default ArticleCard;
+// Helper function to format the date
+function formatDate(dateString) {
+  const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
+  const date = new Date(dateString);
+  return date.toLocaleDateString(undefined, options);
+}
+
+// Helper function to get excerpt
+function getExcerpt(content) {
+  if (!content) return '';
+  return content.length > 150 ? content.substring(0, 147) + '...' : content;
+}
+
+export default ArticleCard;
