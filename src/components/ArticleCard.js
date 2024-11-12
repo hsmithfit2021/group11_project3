abcc43c5761b4ba5f7fcbc6eda1bd20379556101
Tiromachelan <timothys11@outlook.com>
2024-11-12 12:19:36 -0500

    Revert "Update ArticleCard.js"
    This reverts commit f930d32b3a925791bd05c0d74d8a80312f219181.

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -3,24 +3,21 @@
 import '../style/ArticleCard.css';
 import {useState} from "react";
 import FullArticle from './FullArticle';
-import React from 'react';
 
-function ArticleCard({ article, openArticle }) {
-  // Define the variables for sentence, word, and syllable counts
-  const sentenceCount = countSentences(article.content);
-  const wordCount = countWords(article.content);
-  const syllableCount = countSyllables(article.content);
+function ArticleCard({ article, onClick }) {
+  /*const showFull = useState(false);
+  const cardClick = () => {
+    setShowFull(!showFull)
+  }*/
   
+  // Use the article prop to display article data
   return (
-    <div className="proj-card" onClick={() => openArticle(article)}>
+    <div className="proj-card" /*onClick={onClick}*/ onClick = {() => onClick(article)}>
       <img src={article.large_image_url} alt={article.title} />
       <a href={article.url}>{article.domain}</a>
       <h4>{article.title}</h4>
       <p className="date">{formatDate(article.published_date)}</p>
       <p className="desc">{getExcerpt(article.content)}</p>
-      <p className="metrics">
-        Sentences: {countSentences(article.content)} | Words: {countWords(article.content)} | Syllables: {countSyllables(article.content)}
-      </p>
     </div>
   );
 }
